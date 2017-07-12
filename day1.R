0fd5ec8ad19c7de79dfe91007706ebcc63b12f3e
Matthew Bartek <matthew.bartek@gmail.com>
2017-07-12 05:36:49 -0700

    end of day 2

--- day1.R
+++ day1.R
@@ -3,3 +3,19 @@ ufo_data_complete <- read_csv("~/OneDrive - UW Office 365/Learning/Biostats Inst
 cange
 
 #Day 2
+
+##Lecture on Advanced Data I/O
+sheets_url = "https://docs.google.com/spreadsheets/d/18KQQd4LY5k8Ucux1MvWCsQGQJlvd0ECTnn-3ixdOKFM/pubhtml"
+gsurl1 = gs_url(sheets_url)
+dat = gs_read(gsurl1)
+View(dat)
+
+##Lab
+sheets_url = "https://docs.google.com/spreadsheets/d/1DFNjIZsM_-KzUGooYYf0gEPwDCmQbRUS7n-yE71HLL8/pubhtml"
+gsurl1 = gs_url(sheets_url)
+data_2017 = gs_read(gsurl1)
+View(data_2017)
+
+#how many people in the class
+dim(data_2017)
+
